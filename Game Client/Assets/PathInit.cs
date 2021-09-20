8ce560402d84f43628985b0ebb292942f3394464
steppovver <steppovver2@gmail.com>
2021-09-20 23:16:09 +0300

    added map

---
+++ Game Client/Assets/PathInit.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathInit : MonoBehaviour
+{
+    // SINGLETON
+    private static PathInit _instance;
+
+    public static PathInit Instance { get { return _instance; } }
+
+    public Step[] PathStep;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        PathStep = GetComponentsInChildren<Step>();
+
+        for (int i = 0; i < PathStep.Length; i++)
+        {
+            PathStep[i].myIndex = i;
+        }
+    }
+}
